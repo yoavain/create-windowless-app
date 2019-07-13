f39c9512477e72b564073eba5ef94aab4baf09f5
Yoav Vainrich <yoavain@gmail.com>
2019-07-13 08:26:43 +0300

    * Add winston logger to templates

--- templates/javascript/src/index.js
+++ templates/javascript/src/index.js
@@ -1,3 +1,25 @@
+const winston = require('winston');
 const notifier = require('node-notifier');
 
+// Logger init
+const { combine, timestamp, printf, label } = winston.format;
+const transports = {
+    file: new winston.transports.File({ filename: "app.log" })
+};
+transports.file.level = "debug";
+const logger = winston.createLogger({
+    level: 'debug',
+    format: combine(
+        label({ label: '[my-label]' }),
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+        printf(info => `${ info.timestamp } [${ info.level.toUpperCase() }] ${ info.message }`)
+    ),
+    transports: [transports.file]
+});
+
+
+// Log message
+logger.log("info", "Hello World");
+
+// Notify
 notifier.notify('Hello World');
--- templates/typescript/src/index.ts
+++ templates/typescript/src/index.ts
@@ -1,3 +1,25 @@
+import * as winston from 'winston';
 import notifier from 'node-notifier';
 
+// Logger init
+const {combine, timestamp, printf, label} = winston.format;
+const transports = {
+    file: new winston.transports.File({filename: "app.log"})
+};
+transports.file.level = "debug";
+const logger: winston.Logger = winston.createLogger({
+    level: 'debug',
+    format: combine(
+        label({label: '[my-label]'}),
+        timestamp({format: 'YYYY-MM-DD HH:mm:ss.SSS'}),
+        printf(info => `${info.timestamp} [${info.level.toUpperCase()}] ${info.message}`)
+    ),
+    transports: [transports.file]
+});
+
+
+// Log message
+logger.log("info", "Hello World");
+
+// Notify
 notifier.notify('Hello World');
