8a24761c4ed26c01e8108c547efc9f86e222bd96
Yoav Vainrich <yoavain@gmail.com>
2019-11-09 08:26:25 +0200

    prettier-eslint on tests

--- test/launcherCompiler.test.ts
+++ test/launcherCompiler.test.ts
@@ -1,9 +1,9 @@
-import { checkCscInPath } from "../templates/typescript/launcher/launcherCompiler";
-import mockedEnv from "mocked-env";
-import mockFs from "mock-fs";
+import { checkCscInPath } from '../templates/typescript/launcher/launcherCompiler';
+import mockedEnv from 'mocked-env';
+import mockFs from 'mock-fs';
 
-describe("Test checkCscInPath", () => {
-    it("Test CSC found", () => {
+describe('Test checkCscInPath', () => {
+    it('Test CSC found', () => {
         let restoreEnv = mockedEnv({
             PATH: 'fakePath'
         });
@@ -11,22 +11,20 @@ describe("Test checkCscInPath", () => {
             'fakePath/csc.exe': 'exist'
         });
 
-        return checkCscInPath(false)
-            .then(result => {
-                expect(result).toBeTruthy();
-                restoreEnv();
-                mockFs.restore();
-            })
+        return checkCscInPath(false).then((result) => {
+            expect(result).toBeTruthy();
+            restoreEnv();
+            mockFs.restore();
+        });
     });
-    it("Test CSC not found", () => {
+    it('Test CSC not found', () => {
         let restoreEnv = mockedEnv({
             PATH: 'fakePath'
         });
 
-        return checkCscInPath(false)
-            .then(result => {
-                expect(result).toBeFalsy();
-                restoreEnv();
-            })
-    })
+        return checkCscInPath(false).then((result) => {
+            expect(result).toBeFalsy();
+            restoreEnv();
+        });
+    });
 });
