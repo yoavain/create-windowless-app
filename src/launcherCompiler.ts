3abbf0415294ddb7dfdcb64df0e387802f0a647f
Yoav Vainrich <yoavain@gmail.com>
2020-03-28 23:17:34 +0300

    Refactor everything * More refactor...

--- src/launcherCompiler.ts
+++ src/launcherCompiler.ts
@@ -1,13 +1,14 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import spawn from "cross-spawn";
-import { ChildProcess } from "child_process";
+import type { SpawnSyncReturns } from "child_process";
 
-export async function checkCscInPath(exit?: boolean): Promise<boolean> {
+export const checkCscInPath = async (exit?: boolean): Promise<boolean> => {
     // Check for csc.exe in %PATH%
     const promises = process.env.path.split(";").map((p) => fs.pathExists(path.resolve(p, "csc.exe")));
     const results: boolean[] = await Promise.all(promises);
     const cscFound: boolean = results.find((result) => !!result);
+
     if (exit && !cscFound) {
         console.error("You need \"csc.exe\" (C# compiler) in your path in order to compile the launcher.exe.");
         process.exit(1);
@@ -15,21 +16,14 @@ export async function checkCscInPath(exit?: boolean): Promise<boolean> {
     else {
         return cscFound;
     }
-}
+};
 
-export function compileLauncher(sourceLocation: string, outputLocation: string, iconLocation: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-        const command = "csc.exe";
-        const args: string[] = ["/t:winexe", `/out:${outputLocation}`, `/win32icon:${iconLocation}`, `${sourceLocation}`];
-        const child: ChildProcess = spawn(command, args, { stdio: "inherit" });
-        child.on("close", (code) => {
-            if (code !== 0) {
-                reject({
-                    command: `${command} ${args.join(" ")}`
-                });
-                return;
-            }
-            resolve();
-        });
-    });
-}
+export const compileLauncher = async (sourceLocation: string, outputLocation: string, iconLocation: string): Promise<void> => {
+    const command = "csc.exe";
+    const args: string[] = ["/t:winexe", `/out:${outputLocation}`, `/win32icon:${iconLocation}`, `${sourceLocation}`];
+
+    const spawnResult: SpawnSyncReturns<Buffer> = spawn.sync(command, args, { stdio: "inherit" });
+    if (spawnResult.status !== 0) {
+        throw new Error(`${command} ${args.join(" ")}`);
+    }
+};
--- src/nodeUtils.ts
+++ src/nodeUtils.ts
@@ -1,6 +1,8 @@
 import chalk from "chalk";
 import semverCompare from "semver-compare";
 import request from "request";
+import spawn from "cross-spawn";
+import { SpawnSyncReturns } from "child_process";
 
 export const checkNodeVersion = (nodeVersion?: string): Promise<string> => {
     return new Promise<string>((resolve) => {
@@ -53,4 +55,52 @@ export const checkNodeVersion = (nodeVersion?: string): Promise<string> => {
             resolve(nexeNodeVersion);
         });
     });
+};
+
+export const checkThatNpmCanReadCwd = (): boolean => {
+    const cwd = process.cwd();
+    let childOutput: string = null;
+    try {
+        const spawnResult: SpawnSyncReturns<string> = spawn.sync("npm", ["config", "list"]);
+        if (spawnResult.status !== 0) {
+            return false;
+        }
+        childOutput = spawnResult.output.join("");
+    }
+    catch (err) {
+        return false;
+    }
+
+    const lines: string[] = childOutput.split("\n");
+    // `npm config list` output includes the following line:
+    // "; cwd = C:\path\to\current\dir" (unquoted)
+    // I couldn't find an easier way to get it.
+    const prefix = "; cwd = ";
+    const line = lines.find((line) => line.indexOf(prefix) === 0);
+    if (typeof line !== "string") {
+        // Fail gracefully. They could remove it.
+        return true;
+    }
+    const npmCWD = line.substring(prefix.length);
+    if (npmCWD === cwd) {
+        return true;
+    }
+    console.error(
+        chalk.red(
+            "Could not start an npm process in the right directory.\n\n" +
+            `The current directory is: ${chalk.bold(cwd)}\n` +
+            `However, a newly started npm process runs in: ${chalk.bold(npmCWD)}\n\n` +
+            "This is probably caused by a misconfigured system terminal shell."
+        )
+    );
+    if (process.platform === "win32") {
+        console.error(
+            chalk.red("On Windows, this can usually be fixed by running:\n\n") +
+            `  ${chalk.cyan("reg")} delete "HKCU\\Software\\Microsoft\\Command Processor" /v AutoRun /f\n` +
+            `  ${chalk.cyan("reg")} delete "HKLM\\Software\\Microsoft\\Command Processor" /v AutoRun /f\n\n` +
+            chalk.red("Try to run the above two lines in the terminal.\n") +
+            chalk.red("To learn more about this problem, read: https://blogs.msdn.microsoft.com/oldnewthing/20071121-00/?p=24433/")
+        );
+    }
+    return false;
 };
