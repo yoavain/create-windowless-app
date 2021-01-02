2e4a7b834270e58af16a3d80a35729a5bd51dbd4
Yoav Vainrich <yoavain@gmail.com>
2021-01-02 09:48:19 +0200

    Refactor cli-parser with tests (preparation for upgrading commander breaking changes)

---
+++ src/cliParser.ts
@@ -0,0 +1,40 @@
+import type { Command } from "commander";
+import { default as commander } from "commander";
+import chalk from "chalk";
+import { PACKAGE_JSON_FILENAME } from "./createWindowlessAppUtils";
+
+const packageJson = require(`../${PACKAGE_JSON_FILENAME}`);
+
+export type ParsedCommand = {
+    projectName: string,
+    command: Command
+}
+
+export const parseCommand = (argv: string[]): ParsedCommand => {
+    let projectName: string = undefined;
+    const command: Command = new commander.Command(packageJson.name)
+        .version(packageJson.version)
+        .arguments("<project-directory>")
+        .usage(`${chalk.green("<project-directory>")} [options]`)
+        .action((name) => {
+            projectName = name;
+        })
+        .option("--verbose", "print additional logs")
+        .option("--info", "print environment debug info")
+        .option("--interactive", "interactive mode")
+        .option("--no-typescript", "use javascript rather than typescript")
+        .option("--no-husky", "do not install husky pre-commit hook for building launcher")
+        .option("--skip-install", "write dependencies to package.json without installing")
+        .option("--icon <icon>", "override default launcher icon file")
+        .option("--node-version <nodeVersion>", "override node version to bundle")
+        .allowUnknownOption()
+        .on("--help", () => {
+            console.log(`    Only ${chalk.green("<project-directory>")} is required.`);
+            console.log();
+            console.log("    If you have any problems, do not hesitate to file an issue:");
+            console.log(`      ${chalk.cyan("https://github.com/yoavain/create-windowless-app/issues/new")}`);
+            console.log();
+        })
+        .parse(argv);
+    return { projectName, command };
+};
---
+++ test/cliParser.test.ts
@@ -0,0 +1,132 @@
+import { parseCommand } from "../src/cliParser";
+import { v4 as uuid } from "uuid";
+
+describe("Test cliParser", () => {
+    it("should parse default flags", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should parse flags: --skip-install", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox, "--skip-install"]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toEqual(true);
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should parse flags: --no-typescript", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox, "--no-typescript"]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(false);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should parse flags: --no-husky", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox, "--no-husky"]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(false);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should parse flags: --verbose", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox, "--verbose"]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toEqual(true);
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should parse flags: --icon", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox, "--icon", "someIcon"]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toEqual("someIcon");
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should parse flags: --node-version", async () => {
+        const sandbox: string = uuid();
+        const { projectName, command } = parseCommand(["node.exe", "dummy.ts", sandbox, "--node-version", "12.12.0"]);
+        expect(projectName).toEqual(sandbox);
+        expect(command.info).toBeUndefined();
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toEqual("12.12.0");
+    });
+
+    it("should print info with flags: --info", async () => {
+        const { command } = parseCommand(["node.exe", "dummy.ts", "--info"]);
+        expect(command.info).toEqual(true);
+        expect(command.interactive).toBeUndefined();
+        expect(command.verbose).toBeUndefined();
+        expect(command.typescript).toEqual(true);
+        expect(command.husky).toEqual(true);
+        expect(command.skipInstall).toBeUndefined();
+        expect(command.icon).toBeUndefined();
+        expect(command.nodeVersion).toBeUndefined();
+    });
+
+    it("should print help with flags: --help", async () => {
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const mockExit = jest.spyOn(process, "exit").mockImplementation(() => {});
+        // @ts-ignore
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        const mockStdout = jest.spyOn(process.stdout, "write").mockImplementation(() => {});
+        const sandbox: string = uuid();
+        parseCommand(["node.exe", "dummy.ts", sandbox, "--help"]);
+        expect(mockExit).toHaveBeenCalledWith(0);
+        expect(mockStdout.mock.calls[0][0]).toMatchSnapshot("command-help");
+    });
+
+    it("should parse missing project name", async () => {
+        const { projectName } = parseCommand(["node.exe", "dummy.ts"]);
+        expect(projectName).toBeUndefined();
+    });
+});
