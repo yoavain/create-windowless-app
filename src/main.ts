3d938b2e81317f379c1f86644dc3bc7cafadd130
Yoav Vainrich <yoavain@gmail.com>
2022-03-09 06:42:21 +0200

    Fix enforcing minimum NodeJS requirement to 12.20

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { createWindowlessApp } from "./createWindowlessApp";
 export const main = async () => {
     const currentNodeVersion: string = process.versions.node;
     const semver: string[] = currentNodeVersion.split(".");
-    const major = Number(semver[0]);
-    const minor = Number(semver[1]);
+    const major: number = Number(semver[0]);
+    const minor: number = Number(semver[1]);
 
     if (isNaN(major) || isNaN(minor) || major < 12 || (major === 12 && minor < 20)) {
         console.error(`You are running NodeJS ${currentNodeVersion}.\nCreate Windowless App requires NodeJS 12.20 or higher.\nPlease update your version of Node.`);
