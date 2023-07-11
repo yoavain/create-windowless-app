8c1583f66cea68dccc8e495f48217881fd0d59c9
Yoav Vainrich <yoavain@gmail.com>
2023-07-12 01:13:03 +0300

    Extract checkNodeRuntimeVersion to a function (todo - add it to template to run on prebuild)

---
+++ src/checkNodeVersion.ts
@@ -0,0 +1,14 @@
+const MIN_MAJOR_VERSION = 20;
+const MIN_MINOR_VERSION = 0;
+
+export const checkNodeRuntimeVersion = () => {
+    const currentNodeVersion: string = process.versions.node;
+    const semver: string[] = currentNodeVersion.split(".");
+    const major: number = Number(semver[0]);
+    const minor: number = Number(semver[1]);
+
+    if (Number.isNaN(major) || Number.isNaN(minor) || major < MIN_MAJOR_VERSION || (major === MIN_MAJOR_VERSION && minor < MIN_MINOR_VERSION)) {
+        console.error(`You are running NodeJS ${currentNodeVersion}.\nCreate Windowless App requires NodeJS ${MIN_MAJOR_VERSION}.${MIN_MINOR_VERSION} or higher.\nPlease update your version of Node.`);
+        process.exit(1);
+    }
+};
---
+++ test/checkNodeVersion.test.ts
@@ -0,0 +1,30 @@
+import { checkNodeRuntimeVersion } from "../src/checkNodeVersion";
+
+describe("Test checkNodeRuntimeVersion", () => {
+    let p;
+    beforeEach(() => {
+        p = process;
+    });
+    afterEach(() => {
+        jest.restoreAllMocks();
+        process = p;
+    });
+
+
+    describe("Test checkNodeRuntimeVersion", () => {
+        it("should succeed on allowed version", async () => {
+            process = { ...process, versions: { ...process.versions, node :"20.0.0" } };
+            checkNodeRuntimeVersion();
+        });
+
+        it("should exit with code 1 when version does not meet requirements", async () => {
+            // @ts-ignore
+            jest.spyOn(process, "exit").mockImplementation(() => {});
+
+            process = { ...process, versions: { ...process.versions, node :"10.0.0" } };
+            checkNodeRuntimeVersion();
+            expect(process.exit).toHaveBeenCalledTimes(1);
+            expect(process.exit).toHaveBeenCalledWith(1);
+        });
+    });
+});
