cfa7cf64f13faa135273336abaf2fd7b9fd37110
Yoav Vainrich <yoavain@gmail.com>
2023-07-19 22:21:52 +0300

    v11
    * Warn if signtool not in "Path", but not fail
    * Update dependencies

--- src/packageJson/packageJsonConsts.ts
+++ src/packageJson/packageJsonConsts.ts
@@ -12,7 +12,7 @@ const getSingleExecutableApplicationsScripts = (appName: string): Scripts => ({
     "prenode-sea:build-blob": "rimraf _blob && mkdir _blob",
     "node-sea:build-blob": "node --experimental-sea-config sea-config.json",
     "node-sea:copy-node": `node -e "require('fs').copyFileSync(process.execPath, 'dist/${appName}.exe')"`,
-    "node-sea:unsign": `signtool remove /s dist/${appName}.exe`,
+    "node-sea:unsign": `signtool remove /s dist/${appName}.exe || echo Warning: signtool not found in "Path"`,
     "node-sea:inject-blob": `postject dist/${appName}.exe NODE_SEA_BLOB _blob\\sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2`,
     "node-sea:sign": `signtool sign /fd SHA256 dist/${appName}.exe`,
     "node-sea": "npm run node-sea:build-blob && npm run node-sea:copy-node && npm run node-sea:unsign && npm run node-sea:inject-blob"
